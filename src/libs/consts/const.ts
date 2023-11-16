e62b777f35bacc3643fcf75718c8015499690e74
Amir <mikelsonj000@gmail.com>
2023-11-16 21:01:01 +0400

    modify code

--- src/libs/consts/const.ts
+++ src/libs/consts/const.ts
@@ -4,7 +4,7 @@ export const initialize = 'Data Source has been initialized'
 export const twoHour =  2 * 60 * 60 * 1000
 
 export const errors = {
-  formatFile : "Only PDF files are allowed",
+  formatFile : "Only PDF,JPG,JPEG or PNG, files are allowed",
   uploadFile : "No file uploaded",
   internalServer : "Internal Server Error",
   initialize : "Data source initialized error ",
